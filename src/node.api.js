c69f0db0b49776066af525b9bef5b9a1a9740517
satish <satishattada@gmail.com>
2024-07-10 13:14:20 +0100

    updated

--- src/node.api.js
+++ src/node.api.js
@@ -30,7 +30,7 @@ export default ({
     const globExtensions = [...config.extensions, ...extensions]
       .map((ext) => `${ext.slice(1)}`) // cut off the period of the extension
       .join(","); // join them for the glob string
-    const pagesGlob = nodePath.join(location, `*.{${globExtensions}}`);
+    const pagesGlob = nodePath.join(location, '**', `*.{${globExtensions}}`);
     // Get the pages
 
     if (debug) {
